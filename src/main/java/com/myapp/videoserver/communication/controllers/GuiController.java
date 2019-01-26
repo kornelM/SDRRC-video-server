c3fb5b8b5c7d40c7e30e5cea9e5a45e627e43612
kornelM <makusz07@gmail.com>
2019-01-26 21:37:52 +0100

    Add controller methods for rho, max/min gap & lines length

--- src/main/java/com/myapp/videoserver/communication/controllers/GuiController.java
+++ src/main/java/com/myapp/videoserver/communication/controllers/GuiController.java
@@ -12,18 +12,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/gui")
 public class GuiController {
 
+    private final ApplicationContext context;
+
     @Autowired
-    private ApplicationContext context;
+    public GuiController(ApplicationContext context) {
+        this.context = context;
+    }
 
     @PutMapping(value = "/threshold/bottom")
-    public void updateThresholdBottomValue(@RequestBody String thresholdBottom){
+    public void updateThresholdBottomValue(@RequestBody String thresholdBottom) {
         ImageService bean = context.getBean(ImageService.class);
         bean.setThresholdBottom(Integer.parseInt(thresholdBottom));
     }
 
     @PutMapping(value = "/threshold/top")
-    public void updateThresholdTopValue(@RequestBody String thresholdTop){
+    public void updateThresholdTopValue(@RequestBody String thresholdTop) {
         ImageService bean = context.getBean(ImageService.class);
-        bean.setThresholdBottom(Integer.parseInt(thresholdTop));
+        bean.setThresholdTop(Integer.parseInt(thresholdTop));
+    }
+
+    @PutMapping(value = "/threshold/houghLinesP")
+    public void updateHoughLinesPThresholdValue(@RequestBody String houghLinesP) {
+        ImageService bean = context.getBean(ImageService.class);
+        bean.setHoughLinesPThreshold(Integer.parseInt(houghLinesP));
+    }
+
+    @PutMapping(value = "/minLineLength")
+    public void updateMinLineLengthValue(@RequestBody String minLineLength) {
+        ImageService bean = context.getBean(ImageService.class);
+        bean.setMinLineLength(Double.parseDouble(minLineLength));
+    }
+
+    @PutMapping(value = "/maxLineGap")
+    public void updateMaxLineGapValue(@RequestBody String maxLineGap) {
+        ImageService bean = context.getBean(ImageService.class);
+        bean.setMaxLineGap(Double.parseDouble(maxLineGap));
+    }
+
+    @PutMapping(value = "/rho")
+    public void updateRhoValue(@RequestBody String rho) {
+        ImageService bean = context.getBean(ImageService.class);
+        bean.setRho(Double.parseDouble(rho));
     }
 }
